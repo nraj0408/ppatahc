bcfeaab400825026f8a875ee799c8f6b1ab61670
nraj0408 <raikarnagarj1@gmail.com>
2023-06-06 23:25:39 +0530

    frontend added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express =require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
 
 
 const app=express();
 dotenv.config();
+connectDB();
 
 //port value selected from env file
 const port=process.env.PORT || 9000;
